6481e8871af3eee8880cd85c4bebe58d80f4f955
Tania Georgieva <tpgrudeva@uni-sofia.bg>
2021-06-15 17:23:21 +0300

    Added exercises

---
+++ C#Advanced/FunctionalProgramming/CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => w.StartsWith(char.ToUpper(w[0])))
+                .ToArray();
+
+            //Predicate<string> predicate = w => w.ToUpper()[0] == w[0];
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
